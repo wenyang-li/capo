52563cfac9abacd33498eb4a719071cfc2c7149d
wenyang-li <wenyang_li@brown.edu>
2016-09-21 19:22:01 -0400

    bug fix

--- wyl/scripts/omni_run_multi.py
+++ wyl/scripts/omni_run_multi.py
@@ -160,6 +160,10 @@ def calibration(infodict):#dict=[filename, g0, timeinfo, d, f, ginfo, freqs, pol
         SH = (ginfo[1],ginfo[2])
         for p in g0.keys():
             for i in g0[p]: g0[p][i] = numpy.resize(g0[p][i],SH)
+    else:
+        SH = (ginfo[1],ginfo[2])
+        for iant in range(0, ginfo[0]):
+            if not g0[polar[0]].has_key(iant): g0[polar[0]][iant] = numpy.ones(SH)
 
     t_jd = timeinfo['times']
     t_lst = timeinfo['lsts']
