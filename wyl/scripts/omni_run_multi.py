9d8509dd67588bc6d1b1292da9dd4ed5f5cde3fa
wenyang-li <wenyang_li@brown.edu>
2017-02-19 23:42:39 -0500

    bug fix

--- wyl/scripts/omni_run_multi.py
+++ wyl/scripts/omni_run_multi.py
@@ -260,6 +260,8 @@ def calibration(infodict):#dict=[filename, g0, timeinfo, d, f, ginfo, freqs, pol
             proj = np.exp(lp[p[0]]['eta']+1j*(dx*lp[p[0]]['phix']+dy*lp[p[0]]['phiy']+lp[p[0]]['offset']))
             g2[p[0]][a] *= proj
             g2[p[0]][a] = np.resize(g2[p[0]][a],(ginfo[1],ginfo[2]))
+            for ff in range(384):
+                g2[p[0]][a][:,ff] = 0    #clean nans
     ###########################################################################################
     m2['history'] = 'OMNI_RUN: '+''.join(sys.argv) + '\n'
     m2['jds'] = t_jd
